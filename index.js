202618c01728297fde2f67be096ca2873e8ecfae
George  Pianka <gpianka1@gmail.com>
2019-04-27 05:11:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function produceDrivingRange(blockRange){
   return function(firstBlock, secondBlock){
     let distance = Math.abs(parseInt(firstNum) - parseInt(secondNum));
      if (distance <= blockRange) {
-            return `within range by ${blockRange - delta}`
-        } else {
+            return `within range by ${blockRange - distance}`
+     } else {
             return `${delta - blockRange} blocks out of range`
         }
   }
