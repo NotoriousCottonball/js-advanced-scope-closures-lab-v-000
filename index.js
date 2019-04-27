d2e101f85bf1039ee171aa56538a0976ee1682ec
George  Pianka <gpianka1@gmail.com>
2019-04-27 05:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function produceDrivingRange(blockRange){
   return function(firstBlock, secondBlock){
-    let x = parseInt(firstNum); //10 
-    let y = parseInt(secondNum); //12
-    let distance = Math.abs(y - x); //2
+    let distance = Math.abs(parseInt(firstNum) - parseInt(secondNum));
     let difference = Math.abs(distance - blockRange); //10 
     if (distance > blockRange) { //2 > 12?
       return `${difference} blocks out of range`;}
